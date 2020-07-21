0fc56ec20e90c7366546d47ffebec5a0d534927c
Zheng Lu <zheng.lu@salesforce.com>
2020-07-21 16:34:19 -0700

    added functions of reading form parquet

--- reader.py
+++ reader.py
@@ -1,5 +1,7 @@
+import pyarrow as pa
 import pyarrow.parquet as pq
 
+import numpy as np
 import pandas as pd
 import os
 import glob
@@ -7,17 +9,25 @@ import glob
 
 class Reader:
     def __init__(self, path):
-        self.files = glob.glob(os.path.join(path, "**/*.parquet"), recursive=True)
+        self.file_names = glob.glob(os.path.join(path, "**/*.parquet"), recursive=True)
 
     def list_files(self):
-        for file in self.files:
+        for file in self.file_names:
             with open(file) as f:
                 print(f.name)
+
+    def read_parquet_file(self):
+        for file_name in self.file_names:
+            df = pq.read_pandas(file_name, columns=["ipsource"]).to_pandas()
+
+            if not df.empty:
+                df.to_csv("./results.csv", mode='a', header=False)
 
 
 def main():
     reader = Reader("/Users/zheng.lu/csirt_query_7713034/stems")
-    reader.list_files()
+    # reader.list_files()
+    reader.read_parquet_file()
 
 
 if __name__ == "__main__":
